f5b74eb27a693c118c0f980dd10107d068e23fcb
jeyaseelan windows 10 <jeyaseelan.work@gmail.com>
2023-08-08 10:39:54 +0530

    all test cases passed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from './components/Home'
 import Login from './components/Login'
 import Jobs from './components/Jobs'
 import NotFound from './components/NotFound'
-import JobDetails from './components/JobDetails'
+import JobItemDetails from './components/JobItemDetails'
 import ProtectedRoute from './components/ProtectedRoute'
 
 import './App.css'
@@ -60,7 +60,7 @@ const App = () => (
       <Route exact path="/login" component={Login} />
       <ProtectedRoute exact path="/" component={Home} />
       <ProtectedRoute exact path="/jobs" component={Jobs} />
-      <ProtectedRoute exact path="/jobs/:id" component={JobDetails} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
       <Route exact path="/not-found" component={NotFound} />
       <Redirect to="/not-found" />
     </Switch>
